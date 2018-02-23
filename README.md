41d847d2d69136ca1809fd081cd81b1cf758469a
dprophecyguy <dprophecyguy@gmail.com>
2018-02-23 17:31:13 +0530

    Made changes in README.md and first.c

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# theBasics
+# TheBasics
+
+> "What I cannot create, I do not Understand" - Richard Feynman
+
 Going Back To The Root and Trying to Understand Why and What of little bit of Everything.
+
+> Following the Lets Build a Simple Database tutorial to learn about how DB works
+
+[Course Link](https://cstack.github.io/db_tutorial/parts/part1.html
+
+## What to Expect ?
+* I'll be following the approach to finish the tutorial alongside also learning all the side knowledge to make sense of what is happening in the code.
+* I'll create side branches for different things i'll learn i'll try to attach code to them too so those those make sense to you too.
+* I'll try to make the course in a time travel way where you will start from lesson 1 along side different things will start including in the code.
+
+## Progress
+* [x] Part 1 - Introduction and Setting up the REPL
+* [ ] Part 2 - World's Simplest SQL Compiler and Virtual Machine
+* [ ] Part 3 - An In-Memory, Append-Only, Single-Table Database
+* [ ] Part 4 - Our First Tests (and Bugs)
+* [ ] Part 5 - Persistence to Disk
+* [ ] Part 6 - The Cursor Abstraction
+* [ ] Part 7 - Introduction to the B-Tree
+* [ ] Part 8 - B-Tree Leaf Node Format
+* [ ] Part 9 - Binary Search and Duplicate Keys
+* [ ] Part 10 - Splitting a Leaf Node
+* [ ] Part 11 - Recursively Searching the B-Tree
+* [ ] Part 12 - Scanning a Multi-Level B-Tree
+* [ ] Part 13 - Updating Parent Node After a Split
--- first.c
+++ first.c
@@ -1,3 +1,68 @@
+#include <stdtool.h>
+#include <stdio.h
+#include <stdlib.h>
+#include <string.h>
+
+// Define Implementation of InputBuffer
+struct InputBuffer_t {
+  char* buffer;
+  // size_t in c means an unsigned 16 bit int
+  size_t buffer_length;
+  // ssize_t means short size but is signed type
+  ssize_t input_length;
+};
+typedef struct InputBuffer_t InputBuffer;
+
+// new_input_buffer will create a instance of new_input_buffer
+InputBuffer* new_input_buffer() {
+  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+// print_prompt will prompt user for an input.
+void print_prompt() { printf("db > ", );}
+
+// read_input will just take input from std/IO
+void read_input(InputBuffer* input_buffer) {
+
+  // getline function take three input getline(&buffer, &size, stdin)
+  // &buffer is the address of the first character position where the input string will be stored
+  // It's not the base address of the buffer, but of the first character in the buffer.
+
+  // &size is the address of the variable that holds the size of the input buffer, another pointer
+
+  // stind is the input file handle.
+  ssize_t bytes_read =
+    getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if(bytes_read <- 0) {
+      printf("Error reading input\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Ignore trailing new line
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read-1] = 0;
+}
+
+
+/*
+lineptr: a pointer to the variable we use to point to the buffer containing the read line.
+n: a pointer to the variable we use to save the size of allocated buffer.
+stream: The input stream to read from. We'll be reading from standard input.
+return value: The number of bytes read, which may be less than the size of the buffer.
+
+We tell getline to store the read line in input_buffer->buffer and the size of
+the allocated buffer in input_buffer->buffer_length. We store the return value
+in input_buffer->input_length.
+
+
+*/
+
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
   while (true) {
@@ -11,40 +76,3 @@ int main(int argc, char* argv[]) {
     }
   }
 }
-
-// InputBuffer is a small wrapper around the state we need to store
-struct InputBuffer_t {
-  char* buffer;
-  size_t buffer_length;
-  ssize_t input_length;
-};
-// Ask Mamiji what does this line do ?
-typedef struct InputBuffer_t InputBuffer;
-
-// new_input_buffer will create a instance of new_input_buffer
-InputBuffer* new_input_buffer() {
-  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
-  input_buffer->buffer = NULL;
-  input_buffer->buffer->length = 0;
-  input_buffer->input_length = 0;
-
-  return input_buffer;
-}
-
-// print_prompt will prompt user for an input.
-void print_prompt() { printf("db > ", );}
-
-// getline() function will ask for a user input
-ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-
-/*
-lineptr: a pointer to the variable we use to point to the buffer containing the read line.
-n: a pointer to the variable we use to save the size of allocated buffer.
-stream: The input stream to read from. We'll be reading from standard input.
-return value: The number of bytes read, which may be less than the size of the buffer.
-
-We tell getline to store the read line in input_buffer->buffer and the size of
-the allocated buffer in input_buffer->buffer_length. We store the return value
-in input_buffer->input_length.
-
-*/
